55ed99ca4645e28e9ec276c5fc262e57fa0aa966
Leonardo Armelim <57201164+Leonardo-Nunes-Armelim@users.noreply.github.com>
2020-07-05 19:22:41 -0300

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", enventKey)
-            function enventKey(){
-                let code = 'event key: ' + event.key +
-                ' | event code: ' + event.code + 
-                ' | event keycode: ' + event.keyCode
-                document.getElementById("event-key").innerHTML = code
-            }
